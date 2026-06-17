87f1dc3980b4879651ef894e79bc0d2250afd7b1
Lawrence Dartey <lawrencedartey75@gmail.com>
2026-06-17 01:04:22 -0700

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,33 @@
 public class Algorithms {
+  // The Linear Search Algorithm
+    public static int linearSearch(int[] arr, int target){
+        for(int i =0; i< arr.length; i++){
+            if (arr[i] == target){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+// The Binary Search Algorithm
+public static int binarySearch(int []arr, int target){
+    int start = 0;
+    int end = arr.length - 1;
+
+    while(start<= end){
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == target){
+            return mid;
+        
+        }
+        else if (arr[mid]< target){
+            start = mid + 1;
+        } else{
+            end = mid - 1;
+        }
+    }
+    return -1;
 }
